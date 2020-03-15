415e170fbdd32777e60b20a9eb7a304bd53caf53
Gerard <gerardcastell97@gmail.com>
2020-03-15 23:16:04 +0100

    Updates

---
+++ precache-manifest.190581aa0e90286a5d443d10680aab27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c8a48b23d8ae091a3e5deda4de84e5",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "e99168e9788c9def2072",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2b378196b5c6d8d34cc",
+    "url": "/deploy-react/static/js/2.66b70b51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy-react/static/js/2.66b70b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99168e9788c9def2072",
+    "url": "/deploy-react/static/js/main.1dbdafd0.chunk.js"
+  },
+  {
+    "revision": "58a6f1bb843e2d06b23f",
+    "url": "/deploy-react/static/js/runtime-main.e5e82a80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
